6dd0cca489f41682f27ea4696430a4180470c10d
Takashi Kokubun <takashikkbn@gmail.com>
2014-11-30 21:05:37 +0900

    Complete implementation of map

--- printer.go
+++ printer.go
@@ -1,11 +1,11 @@
 package pp
 
 import (
-	"strings"
 	"bytes"
 	"fmt"
 	"github.com/k0kubun/palette"
 	"reflect"
+	"strings"
 	"text/tabwriter"
 )
 
@@ -14,17 +14,20 @@ const (
 )
 
 func format(object interface{}) string {
+	return newPrinter(object).String()
+}
+
+func newPrinter(object interface{}) *printer {
 	buffer := bytes.NewBufferString("")
 	tw := new(tabwriter.Writer)
 	tw.Init(buffer, indentWidth, 0, 1, ' ', 0)
 
-	p := &printer{
+	return &printer{
 		Buffer: buffer,
 		tw:     tw,
 		depth:  0,
 		value:  reflect.ValueOf(object),
 	}
-	return p.String()
 }
 
 type printer struct {
@@ -59,7 +62,11 @@ func (p *printer) print(text string) {
 }
 
 func (p *printer) println(text string) {
-	p.print(text+"\n")
+	p.print(text + "\n")
+}
+
+func (p *printer) indentPrint(text string) {
+	p.print(p.indent() + text)
 }
 
 func (p *printer) colorPrint(text, color string) {
@@ -74,18 +81,17 @@ func (p *printer) printString() {
 
 func (p *printer) printMap() {
 	p.println("{")
-	p.indented(func(){
+	p.indented(func() {
 		keys := p.value.MapKeys()
 		for i := 0; i < p.value.Len(); i++ {
 			key := keys[i].Interface()
 			value := p.value.MapIndex(keys[i]).Interface()
-			indent := strings.Repeat("\t", p.depth)
 
-			fmt.Fprintf(p.tw, "%s%s:\t%s,\n", indent, format(key), format(value))
+			fmt.Fprintf(p.tw, "%s%s:\t%s,\n", p.indent(), p.format(key), p.format(value))
 		}
 		p.tw.Flush()
 	})
-	p.println("}")
+	p.indentPrint("}")
 }
 
 func (p *printer) indented(proc func()) {
@@ -96,4 +102,14 @@ func (p *printer) indented(proc func()) {
 
 func (p *printer) raw() string {
 	return fmt.Sprintf("%#v", p.value.Interface())
+}
+
+func (p *printer) format(object interface{}) string {
+	pp := newPrinter(object)
+	pp.depth = p.depth
+	return pp.String()
+}
+
+func (p *printer) indent() string {
+	return strings.Repeat("\t", p.depth)
 }
